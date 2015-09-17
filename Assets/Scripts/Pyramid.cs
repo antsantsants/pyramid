3880da836025fd6710717c6a0f71e16aeb99c69f
brulo <bruce.lott@gmail.com>
2015-09-17 13:39:12 -0700

    Make explosion work

--- Assets/Scripts/Pyramid.cs
+++ Assets/Scripts/Pyramid.cs
@@ -7,11 +7,19 @@ public class Pyramid : MonoBehaviour {
 	public int baseLength = 3;
 	public float spacing = 5f;
 
+	public float explosionRadius;
+	public float explosionUpwardModifier;
+
 	private List<GameObject> copies = new List<GameObject>();
 	private List<Vector3> locs = new List<Vector3>();
+	private bool inPlace = true;
+	private bool interruptReset = false;
+	private bool resetting = false;
+	private Vector3 centerPoint;
 
 	void Start() {
 		Vector3 startPosition = transform.position;
+		centerPoint = startPosition;
 		int layerLength = baseLength;
 
 		for(int y = 0; y < baseLength; y++) {
@@ -30,5 +38,70 @@ public class Pyramid : MonoBehaviour {
 			}				
 			layerLength--;
 		}
+		for(int i = 0; i < copies.Count; i++) {
+			copies[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+		}
 	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.E))
+			Explode();
+	    if(Input.GetKeyDown(KeyCode.R))
+	    	Reset();
+	}
+
+
+	public void Explode() {
+		inPlace = false;
+		interruptReset = true;
+		Vector3 explosionPosition = new Vector3(0f, 
+																						0.8660254037f * spacing, 
+																						0f) + transform.position;
+		for(int i = 0; i < copies.Count; i++) {
+			Rigidbody rb = copies[i].GetComponent<Rigidbody>();
+			rb.constraints = RigidbodyConstraints.None;
+			rb.AddExplosionForce(Random.Range(600f, 1000f), // force
+													 explosionPosition,         // position
+													 1000f,											// radius 
+													 5f); 											// mode
+		}
+	}
+
+  public void Reset() {
+    if(!resetting && !inPlace) {
+      resetting = true;
+      interruptReset = false;
+      for(int z = 0; z < locs.Count; z++){
+        StartCoroutine(MoveObject(copies[z], locs[z], 1.2f));
+        //copies[z].transform.rotation = Quaternion.identity;
+        //copies[z].rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+      }
+    }
+  }
+
+  IEnumerator MoveObject(GameObject gameObject, Vector3 endPosition, float time) {
+    float rate = 1f / time;
+    Vector3 startPosition = gameObject.transform.position;
+    Quaternion startRotation = gameObject.transform.rotation;
+
+    float i = 0f;
+    while (i < 1f) {
+      if(interruptReset) {
+        resetting = false;
+        yield break;
+      }
+      i += Time.deltaTime * rate;
+      gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, i);
+      gameObject.transform.rotation = Quaternion.Slerp(startRotation, Quaternion.identity, i);
+      yield return 0;
+    }
+
+    gameObject.transform.position = endPosition;
+    gameObject.transform.rotation = Quaternion.identity;
+    gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+    resetting = false;
+    inPlace = true;
+  }
+
 }
